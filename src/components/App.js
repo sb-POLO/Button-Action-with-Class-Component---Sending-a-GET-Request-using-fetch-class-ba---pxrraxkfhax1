1e77f0084491925642759a6e7641dbf263f3f1e8
Sourav Banerjee <rjsourav88@gmail.com>
2022-08-23 11:02:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,24 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state = {
+		para: false
 	};
-	state = {
-		flag: false
 	}
+	
+	para(){
+		return(
+		<p id="para">
+			Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+			</p>
+			);
+	}
+	
     render() {
     	return(
     		<div id="main">
-			{this.state.flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
-			<button id="click" onClick={()=> this.setState({flag: true})}>Click</button>
+			<button id="click" onClick={()=> this.setState({para: true})}>Click</button>
+{this.state.para ? this.para() : ""}
     		</div>
     	);
     }
